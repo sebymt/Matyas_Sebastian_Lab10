840f0a5c5348360e994375766df585a2a95b6df9
SEB <SEB@VERSACE>
2020-12-10 22:47:59 +0200

    Add project files.

---
+++ Matyas_Sebastian_Lab10/Models/ShopList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+
+namespace Matyas_Sebastian_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
